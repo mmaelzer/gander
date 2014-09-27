41af6a06bf1dcb5737e5ac3cfc95d896f9d4b731
Mike Maelzer <mmaelzer@gmail.com>
2014-09-27 15:04:08 -0700

    v1.0.1. Cleanup whitespace and remove module name for AMD imports.

--- gander.js
+++ gander.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
   /**
    *  @param {Object} object
    *  @param {Options} options
@@ -12,7 +12,7 @@
    */
   function gander(object, options) {
     options = options || {};
-    
+
     // The name of this object or an internally generated id
     var name = getName(options.name, options.unique);
 
@@ -25,7 +25,7 @@
 
     // Methods to ignore
     var ignore = (options.ignore || []).concat('constructor');
-    
+
     // If no before/after functions provided, default
     // to console.time/console.timeEnd
     if (!options.before && !options.after) {
@@ -42,7 +42,7 @@
       if (typeof object[key] === 'function' && ignore.indexOf(key) === -1) {
         var fn = object[key];
         object[key] = function() {
-          
+
           var args = [];
           // https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#3-managing-arguments
           for (var i = 0; i < arguments.length; i++) {
@@ -67,7 +67,7 @@
               callback.apply(this, callbackArgs);
             };
             return fn.apply(this, args);
-          } 
+          }
           // Handle sync calls
           else {
             var ret = fn.apply(this, args);
@@ -109,17 +109,17 @@
   }
 
   var counter = 0;
-  /** 
+  /**
    *  @param {String=} opt_name
    *  @param {Boolean=} opt_unique
-   *  @return {String} 
+   *  @return {String}
    */
   function getName(opt_name, opt_unique) {
     if (opt_name) {
       return opt_unique ? (opt_name + (++counter)) : opt_name;
     } else {
-      return 'o' + (++counter);  
-    }    
+      return 'o' + (++counter);
+    }
   }
 
   // CommonJS/node.js support
@@ -135,7 +135,7 @@
 
   // AMD/Require.js support
   if (typeof define === 'function' && define.amd) {
-    define('gander', [], function() {
+    define([], function() {
       return gander;
     });
   }
